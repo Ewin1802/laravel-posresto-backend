605ba49f3a3a8fcb89dfe81e421e5ffb14d3a1fb
Ewin1802 <elantapa@gmail.com>
2025-01-10 13:39:03 +0800

    tulisan stisla

--- resources/views/components/error-footer.blade.php
+++ resources/views/components/error-footer.blade.php
@@ -1,3 +1,3 @@
 <div class="simple-footer mt-5">
-    Copyright &copy; Stisla 2018
+    Copyright &copy; EwinLantapa 2025
 </div>
--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no"
         name="viewport">
-    <title>@yield('title') &mdash; Stisla</title>
+    <title>@yield('title') &mdash; Resto ARCH</title>
     {{-- Favicon - Logo web disamping title --}}
     <link rel="icon" href="{{ asset('img/logo_pemda.png') }}" type="image/png">
 
