007b5a687c8844a82ef779bc23dbde507de5fca6
Ewin1802 <elantapa@gmail.com>
2025-01-23 01:52:51 +0800

    api add product

--- routes/api.php
+++ routes/api.php
@@ -31,7 +31,7 @@ Route::post('/logout', [App\Http\Controllers\Api\AuthController::class, 'logout'
 
 //products api
 Route::get('/products', [App\Http\Controllers\Api\ProductController::class, 'index'])->middleware('auth:sanctum');
-Route::post('/products/input', [App\Http\Controllers\Api\ProductController::class, 'store'])->middleware('auth:sanctum');
+Route::post('/products', [App\Http\Controllers\Api\ProductController::class, 'store'])->middleware('auth:sanctum');
 Route::post('/products/edit', [App\Http\Controllers\Api\ProductController::class, 'update'])->middleware('auth:sanctum');
 Route::delete('/products/{id}', [App\Http\Controllers\Api\ProductController::class, 'destroy'])->middleware('auth:sanctum');
 //categories api
