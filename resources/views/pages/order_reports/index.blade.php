192d33e3d6b96d3046d421eb58eb5bb20145858a
Ewin1802 <elantapa@gmail.com>
2025-06-08 20:10:57 +0800

    modal dialog

--- resources/views/pages/order_reports/index.blade.php
+++ resources/views/pages/order_reports/index.blade.php
@@ -278,7 +278,7 @@
 @endpush
 
 <div class="modal fade" id="orderDetailModal" tabindex="-1" aria-labelledby="orderDetailModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
+    <div class="modal-dialog modal-lg-custom">
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title" id="orderDetailModalLabel">Detail Pesanan</h5>
